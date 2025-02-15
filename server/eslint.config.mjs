59adecd1d8b535bccfd5bd515321ba48cd9d94a6
booboojp <68347504+booboojp@users.noreply.github.com>
2025-02-15 00:35:45 -0500

    New: - Added EsLint - Created edlint.config.mjs for /server with custom rules.
    Changed: None
    
    Removed: None
    
    Signed-off-by: booboojp <68347504+booboojp@users.noreply.github.com>

---
+++ server/eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+                process: true,
+                Buffer: true,
+                __dirname: true
+            }
+        },
+        rules: {
+          "no-unused-vars": "error",
+          "no-undef": "error",
+          "no-duplicate-imports": "error",
+          "no-console": ["warn", { allow: ["warn", "error"] }],
+          "prefer-const": "error",
+          "no-var": "error",
+          "eqeqeq": "error",
+          "indent": ["error", "tab"],
+          "quotes": ["error", "single"],
+          "semi": ["error", "always"],
+          "no-multiple-empty-lines": ["error", { "max": 1 }],
+          "space-before-function-paren": ["error", "never"]
+        }
+    },
+    pluginJs.configs.recommended,
+];
