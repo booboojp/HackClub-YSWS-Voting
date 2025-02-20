bc8b67cf3845ebc26281fcf3b1d6472435bd2423
booboojp <68347504+booboojp@users.noreply.github.com>
2025-02-20 18:35:52 -0500

    New: None
    Changed: None
    
    Removed:
    - Removed old Authentication testing JSX React Components.
    
    Signed-off-by: booboojp <68347504+booboojp@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Terminal from './components/Terminal.jsx';
-import Testing from './components/Testing.jsx';
 import '@fontsource/source-code-pro';
 import '@fontsource/source-code-pro';
 import { AuthProvider } from './components/AuthProvider'
